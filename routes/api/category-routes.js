d689afd79fa1a48eded887e868d76518ac969fd5
Viren <144032304+viren15152@users.noreply.github.com>
2024-01-07 21:22:52 +0000

    Updated my category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,8 +4,6 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', async (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
   try {
     const categoriesData = await Category.findAll({
       include: [{ model: Product }],
@@ -18,8 +16,6 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
   try {
     const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }],
@@ -38,7 +34,6 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  // create a new category
   try {
     const newCategory = await Category.create(req.body);
     res.status(200).json(newCategory);
@@ -49,29 +44,31 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-  // update a category by its 'id' value
   try {
     const [updatedRows] = await Category.update(req.body, {
       where: {
         id: req.params.id,
       },
     });
-    
+
     if (updatedRows === 0) {
       res.status(404).json({ message: 'No category found with this id' });
       return;
     }
 
-    res.status(200).json({ message: 'Category updated successfully' });
+    // Fetch the updated category data
+    const updatedCategory = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    res.status(200).json({ message: 'Category updated successfully', updatedCategory });
   } catch (err) {
     console.error(err);
     res.status(500).json(err);
   }
 });
 
-
 router.delete('/:id', async (req, res) => {
-  // delete a category by its 'id' value
   try {
     const deletedCategory = await Category.destroy({
       where: {
@@ -91,4 +88,4 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
-module.exports = router;  
+module.exports = router;
